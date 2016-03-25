c4bc6aecf54b90c65f8db6145f715d6250dc2fb2
David Frank <bitinn@gmail.com>
2016-03-26 01:31:14 +0800

    static file server

--- index.js
+++ index.js
@@ -5,18 +5,22 @@
  * App entry point
  */
 
-var koa = require('koa');
-var logger = require('koa-logger');
-var router = require('koa-router')();
+var koa = require('koa')
+var logger = require('koa-logger')
+var router = require('koa-router')()
+var static = require('koa-static')
 
-var app = koa();
+var app = koa()
 
 router.get('/', function *(next) {
 	this.body = 'hello';
-});
+})
 
-app.use(logger());
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(logger())
+app.use(static('public', {
+	maxage: 1000 * 60 * 60 * 24
+}))
+app.use(router.routes())
+app.use(router.allowedMethods())
 
-app.listen(8080);
+app.listen(8080)
