c2ec307e7e82b13472327859ccda9aca3eb2491e
David Frank <bitinn@gmail.com>
2016-03-26 01:17:39 +0800

    simple server

---
+++ index.js
@@ -0,0 +1,22 @@
+
+/**
+ * index.js
+ *
+ * App entry point
+ */
+
+var koa = require('koa');
+var logger = require('koa-logger');
+var router = require('koa-router')();
+
+var app = koa();
+
+router.get('/', function *(next) {
+	this.body = 'hello world';
+});
+
+app.use(logger());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(8080);
