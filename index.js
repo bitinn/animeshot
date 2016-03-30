613903f36a6b95f9bf08d49ee5f6b85710a26d41
David Frank <bitinn@gmail.com>
2016-03-31 02:35:08 +0800

    basic server upload

--- index.js
+++ index.js
@@ -5,19 +5,36 @@
  * App entry point
  */
 
+var fs = require('fs')
+var cuid = require('cuid')
 var koa = require('koa')
 var logger = require('koa-logger')
 var router = require('koa-router')()
 var static = require('koa-static')
+var busboy = require('co-busboy')
 var templateLoader = require('./templates/marko-template-loader')
 var homeTemplate = templateLoader('./main.marko')
 
 var app = koa()
 
 router.get('/', function *(next) {
+	yield next
 	this.body = homeTemplate.renderSync({
 		name: 'David Frank'
 	})
+})
+
+router.post('/api/files', function *(next) {
+	yield next
+	var body = busboy(this, {
+		autoFields: true
+	})
+	var name = 'public/upload/' + cuid() + '.jpg'
+	var part
+	while (part = yield body) {
+		part.pipe(fs.createWriteStream(name))
+	}
+	this.body = 'done'
 })
 
 app.use(logger())
