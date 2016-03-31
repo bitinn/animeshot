e8619fb68b80a270e1321f5f2cea75a03e89139d
David Frank <bitinn@gmail.com>
2016-03-31 15:33:42 +0800

    working on uploader

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,16 @@
 
-var dz = new Dropzone('div#upload-dropzone', {
-	maxFilesize: 8
-	, 
-});
+'use strict';
+
+Dropzone.autoDiscover = false
+
+$(document).ready(function() {
+	var dz = new Dropzone('#upload-dropzone', {
+		maxFilesize: 8
+		, acceptedFiles: 'image/png,image/jpeg,image/bmp,image/gif'
+		, forceFallback: false
+		, dictDefaultMessage: '上传文件'
+		, dictInvalidFileType: '不支持这类文件'
+		, dictFileTooBig: '文件大小超过上限'
+		, dictResponseError: '服务器出错 >_<'
+	})
+})
