1981039d51d221f1e8b36f0800af103c2e11d776
David Frank <bitinn@gmail.com>
2016-03-31 17:18:31 +0800

    database migration tool

---
+++ migration/migration.js
@@ -0,0 +1,32 @@
+
+/**
+ * migration.js
+ *
+ * Standalone script to migrate database schema
+ */
+
+var co = require('co')
+var databaseUrl = 'mongodb://localhost:27017/animeshot?w=1'
+var mongo = require('yieldb').connect;
+
+function *migration() {
+	console.log('migration started')
+
+	var db = yield mongo(databaseUrl)
+
+	var Shots = db.col('shots')
+
+	yield Shots.index({
+		sid: 1
+	}, {
+		unique: true
+	})
+};
+
+co(migration).then(function() {
+	console.log('migration done')
+	process.exit()
+}).catch(function(err) {
+	console.error(err.stack)
+	process.exit()
+});
