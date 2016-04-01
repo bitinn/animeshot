943d0246dcd3bd69b2822d9b10273de530d6f00e
David Frank <bitinn@gmail.com>
2016-04-02 00:29:20 +0800

    new term of use

--- templates/header.marko.js
+++ templates/header.marko.js
@@ -5,7 +5,7 @@ function create(__helpers) {
       escapeXml = __helpers.x;
 
   return function render(data, out) {
-    out.w("<blocks cols=\"2\" class=\"header\"><div class=\"header-title\"><h1><a href=\"/\">AnimeShot</a></h1></div><div class=\"header-description\"><p>本站仅收集「吐槽向动画字幕截图」，用于广大群众的搜索与吐槽之需。非动画字幕的截图请不要上传，会被管理员删除。</p></div></blocks>");
+    out.w("<blocks cols=\"2\" class=\"header\"><div class=\"header-title\"><h1><a href=\"/\">AnimeShot</a></h1></div><div class=\"header-description\"><p>本站仅收集高质的吐槽向的「动画截图」，用于广大群众的搜索与吐槽之需。非动画的截图请不要上传，会被管理员删除。</p></div></blocks>");
   };
 }
 
