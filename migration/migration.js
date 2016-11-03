a1e894066d423d0ce663a5cfb25640fb5787b4ae
David Frank <bitinn@users.noreply.github.com>
2016-11-03 22:02:12 +0800

    smarter search (#18)
    * smarter search with pinyin token
    * changelog update

--- migration/migration.js
+++ migration/migration.js
@@ -7,13 +7,25 @@
 
 var co = require('co')
 var mongo = require('yieldb').connect
+var pinyin = require('pinyin')
 
 var databaseUrl = 'mongodb://localhost:27017/animeshot?w=1'
+var databaseRevision = 3;
 
 function *migration() {
 	console.log('migration started')
 
 	var db = yield mongo(databaseUrl)
+
+	var Revision = db.col('revision')
+
+	var ver = yield Revision.findOne()
+
+	if (ver && ver.currentVersion >= databaseRevision) {
+		return false
+	}
+
+	yield Revision.update({}, { currentVersion: databaseRevision }, { upsert: true })
 
 	var Shots = db.col('shots')
 
@@ -26,10 +38,24 @@ function *migration() {
 	yield Shots.index({
 		created: -1
 	})
+
+	var shots = yield Shots.find()
+
+	// slow but we will take it for now
+	for (var i = 0; i < shots.length; i++) {
+		var normalized = [].concat.apply([], pinyin(shots[i].text))
+		yield Shots.update({ sid: shots[i].sid }, { normalized: normalized.join(' ') })
+	}
+
+	return true
 }
 
-co(migration).then(function() {
-	console.log('migration done')
+co(migration).then(function(state) {
+	if (state) {
+		console.log('migration done, updated')
+	} else {
+		console.log('migration done, no update')
+	}
 	process.exit()
 }).catch(function(err) {
 	console.error(err.stack)
